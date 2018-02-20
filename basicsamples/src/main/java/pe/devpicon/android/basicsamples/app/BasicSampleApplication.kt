1053567c34bcb65359e73d8afb2043138a90aeba
Armando Picón <picondev@gmail.com>
2018-02-20 11:01:06 -0300

    Added Facebook Stetho support

--- basicsamples/src/main/java/pe/devpicon/android/basicsamples/app/BasicSampleApplication.kt
+++ basicsamples/src/main/java/pe/devpicon/android/basicsamples/app/BasicSampleApplication.kt
@@ -1,6 +1,7 @@
 package pe.devpicon.android.basicsamples.app
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import com.raizlabs.android.dbflow.config.FlowManager
 
 /**
@@ -11,5 +12,6 @@ class BasicSampleApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         FlowManager.init(this)
+        Stetho.initializeWithDefaults(this);
     }
 }
